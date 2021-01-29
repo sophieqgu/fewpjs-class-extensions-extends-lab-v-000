3469bd76641dc595da034cd93fd08a971e356655
Sophie Gu <sophieqgu@gmail.com>
2021-01-29 02:14:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Your code here
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+
+  get countSides() {
+    return this.sides.length;
+  }
+
+  get perimeter() {
+    let sum = 0;
+    for (let s of this.sides) {
+      sum += s;
+    }
+    return sum;
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    if (this.sides.length != 3) {
+      return false;
+    }
+    this.sides.sort();
+    return this.sides[0] + this.sides[1] > this.sides[2];
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    if (this.sides.length != 4) {
+      return false;
+    }
+    let side = this.sides[0];
+    for (let s of this.sides) {
+      if (s !== side) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  get area() {
+    if (this.isValid) {
+      let side = this.sides[0];
+      return side ** 2;
+    }
+    throw "This is not a valid square."
+  }
+}
