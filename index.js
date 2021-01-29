0a10befa8a6a21e3128341ef388ed00f2a7d8696
Sophie Gu <sophieqgu@gmail.com>
2021-01-29 02:01:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 // Your code here
 class Polygon {
   constructur(...sides) {
-    this.sides = sides
+    this.sides = sides;
   }
+
+  get countSides() {
+    return sides.length;
+  }
+
+  get perimeter() {
+    int sum = 0;
+    for (int i = 0; i < sides.length; i++) {
+      sum += sides[i];
+    }
+    return sum;
+  }
+}
+
+class Triangle extends Polygon {
+  
 }
